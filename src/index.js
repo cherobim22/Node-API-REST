9a95d922bf8bc9edf4e8b27831fdc9b61c083215
cherobim22 <lucascherobim@hotmail.com>
2021-03-22 23:06:07 -0300

    update files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const bodyParser = require('bodyParser')
+// const bodyParser = require('body-parser')
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extend: false}))
+// app.use(express.bodyParser());
 
+require('./controllers/authController')(app);
 
 app.listen(3333);
-console.log("rodanado la na 3333")
