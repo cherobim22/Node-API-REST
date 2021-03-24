0d1918e5013d56390a56ae44a2321ff2f6ca30ef
cherobim22 <lucascherobim@hotmail.com>
2021-03-23 22:06:40 -0300

    fix database

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = "mongodb+srv://cherobim:aoaGj1eq15sR71vX@cluster0.krk0w.mongodb.net/teste?retryWrites=true&w=majority";
+const url = "mongodb+srv://<user>:<password>@cluster0.krk0w.mongodb.net/teste?retryWrites=true&w=majority";
 
 mongoose.set('useFindAndModify', false);
 mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
