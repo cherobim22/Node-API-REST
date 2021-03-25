6b443514fc56f003059ce162fcb5bb25751d929d
cherobim22 <lucascherobim@hotmail.com>
2021-03-25 16:30:41 -0300

    inserindo o middleware

---
+++ src/middlewares/auth.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth.json'); 
+
+module.exports = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader){
+        return res.status(401).send({error: "Nenhum token de acesso foi encontrado"});
+    }
+    
+    jwt.verify(authHeader, authConfig.secret, (err, decoded) => {
+        if(err){
+            console.log(err);
+            return res.status(401).send({error: "Token invalido ou expirado"});
+        }
+
+        req.userId = decoded.id;
+        return next();
+    });
+
+
+}
